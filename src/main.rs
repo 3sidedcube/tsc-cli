7143af7c0294a9a24a525fabea45352c546a9c58
M Lau <63963445+tofuuudon@users.noreply.github.com>
2024-10-19 22:18:47 +0100

    feat: add migrate commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Platform {
+    #[command(name = "github")]
+    GitHub {
+        #[arg(short, long)]
+        create_repo: bool,
+
+        #[arg(short, long, default_value = "3sidedcube")]
+        org: String,
+
+        #[arg(help = "Name of the repository")]
+        repo_name: String,
+    },
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Migrate {
+        #[command(subcommand)]
+        platform: Option<Platform>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Migrate { platform }) => match platform {
+            Some(Platform::GitHub {
+                create_repo,
+                org,
+                repo_name,
+            }) => {
+                println!("GitHub");
+                if *create_repo {
+                    println!("Create a repo");
+                }
+                println!("Pointing to {}/{}", org, repo_name);
+                println!("Reconfigure current repository to new repo")
+            }
+            None => {}
+        },
+        None => {}
+    }
 }
