9da7093db7aa24127fa4aba6d50cb2eae32e3ca5
M Lau <63963445+tofuuudon@users.noreply.github.com>
2024-10-21 23:35:27 +0100

    chore(homebrew): add intel condition

--- Formula/tsc-cli.rb
+++ Formula/tsc-cli.rb
@@ -5,6 +5,11 @@ class TscCli < Formula
   sha256 "d01ca75f362f03356f8a2afd0baa269e1aa3e67f2b3d79a1e52621d86bf3f99f"
   version "0.1.0"
 
+  on_intel do
+    url "https://github.com/3sidedcube/tsc-cli/releases/download/v0.1.0/tsc-cli-0.1.0-x86_64-apple-darwin.tar.gz"
+    sha256 "86054de1c7c1570cbc40604d321a774ebad35d1741198876718cda9da3ca6700"
+  end
+
   def install
     bin.install "tsc-cli"
   end
