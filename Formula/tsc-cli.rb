3a06f51c7e4ec252e1a90d0b8994b347be812184
M Lau <63963445+tofuuudon@users.noreply.github.com>
2024-10-21 23:08:18 +0100

    chore(homebrew): update checksum

--- Formula/tsc-cli.rb
+++ Formula/tsc-cli.rb
@@ -2,7 +2,7 @@ class TscCli < Formula
   desc "A CLI tool for 3 Sided Cube"
   homepage "https://github.com/3sidedcube/tsc-cli"
   url "https://github.com/3sidedcube/tsc-cli/releases/download/v0.1.0/tsc-cli-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "27857d227207748b76ffece3508a9192558ff92f"
+  sha256 "86054de1c7c1570cbc40604d321a774ebad35d1741198876718cda9da3ca6700"
   version "0.1.0"
 
   def install
