284300912c02f6b431d91da50c5ae4131b5b1b16
M Lau <63963445+tofuuudon@users.noreply.github.com>
2024-10-21 23:20:36 +0100

    chore(homebrew): remove private repo util

--- Formula/tsc-cli.rb
+++ Formula/tsc-cli.rb
@@ -1,7 +1,7 @@
 class TscCli < Formula
   desc "A CLI tool for 3 Sided Cube"
   homepage "https://github.com/3sidedcube/tsc-cli"
-  url "https://github.com/3sidedcube/tsc-cli/releases/download/v0.1.0/tsc-cli-0.1.0-x86_64-apple-darwin.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/3sidedcube/tsc-cli/releases/download/v0.1.0/tsc-cli-0.1.0-x86_64-apple-darwin.tar.gz"
   sha256 "86054de1c7c1570cbc40604d321a774ebad35d1741198876718cda9da3ca6700"
   version "0.1.0"
 
