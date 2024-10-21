d2ad0f9849366643fcde59143732ef82f82f2525
M Lau <63963445+tofuuudon@users.noreply.github.com>
2024-10-22 00:28:57 +0100

    chore(homebrew): bump formula to 0.1.1 [skip ci]

--- Formula/tsc-cli.rb
+++ Formula/tsc-cli.rb
@@ -1,13 +1,13 @@
 class TscCli < Formula
   desc "A CLI tool for 3 Sided Cube"
   homepage "https://github.com/3sidedcube/tsc-cli"
-  url "https://github.com/3sidedcube/tsc-cli/releases/download/v0.1.0/tsc-cli-0.1.0-aarch64-apple-darwin.tar.gz"
-  sha256 "d01ca75f362f03356f8a2afd0baa269e1aa3e67f2b3d79a1e52621d86bf3f99f"
-  version "0.1.0"
+  url "https://github.com/3sidedcube/tsc-cli/releases/download/v0.1.1/tsc-cli-0.1.1-aarch64-apple-darwin.tar.gz"
+  sha256 "49dc3c9d58c0f396bb7288a41c9dfd0057ee64fecce9e2d103a38b446606f207"
+  version "0.1.1"
 
   on_intel do
-    url "https://github.com/3sidedcube/tsc-cli/releases/download/v0.1.0/tsc-cli-0.1.0-x86_64-apple-darwin.tar.gz"
-    sha256 "86054de1c7c1570cbc40604d321a774ebad35d1741198876718cda9da3ca6700"
+    url "https://github.com/3sidedcube/tsc-cli/releases/download/v0.1.1/tsc-cli-0.1.1-x86_64-apple-darwin.tar.gz"
+    sha256 "8278c93dc0c5b99b3dd256c9c6bc327f2bba2e57d46f9ef48a74aeaa4f4c9262"
   end
 
   def install
